e8d7377aec4da09f1f6ae228cd25eca47ed5fb36
Ales Krot <ales.krot@gmail.com>
2022-02-16 15:34:53 +0300

    Add pods

--- KrotAles_L34_HW/KrotAles_L34_HW/ViewController.swift
+++ KrotAles_L34_HW/KrotAles_L34_HW/ViewController.swift
@@ -6,14 +6,15 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import RxRelay
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
-
 }
-
